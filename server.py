c6210cbe117fec0723291c9d7881f619407bdb99
azka1415 <rafifzaydan4@gmail.com>
2022-08-23 12:59:54 +0700

    added delay

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
+import time
 import uvicorn
 import os
 
 if __name__ == '__main__':
     os.system('docker-compose up -d')
+    time.sleep(2)
     uvicorn.run('src.main:app', port=5000, reload=True)
