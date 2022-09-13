65beca75c81a875fa05fbd012af900c0ef5cabbd
azka1415 <rafifzaydan4@gmail.com>
2022-09-13 17:15:55 +0700

    fixed can't find notes

--- src/database.py
+++ src/database.py
@@ -31,16 +31,15 @@ async def db_parser(cursor: Cursor):
 
 async def fetch_all(current_user: dict, limit: int, skip: int, title: str):
     ''' fetch every note from owner '''
-    if current_user['role'] == 'admin' or current_user['role'] == 'super_admin':
+    if current_user['role'] == 'user':
         cursor = notes_collection.find(
-            {'title': {"$regex": title}}).limit(limit).skip(skip)
+            {'title': {"$regex": title},'owner':current_user['email']}).limit(limit).skip(skip)
         result = await db_parser(cursor)
         return result
-    else:
-        cursor = notes_collection.find(
-            {'title': {"$regex": title}, 'owner': current_user['email']}).limit(limit).skip(skip)
-        result = await db_parser(cursor)
-        return result
+    cursor = notes_collection.find(
+        {'title': {"$regex": title}}).limit(limit).skip(skip)
+    result = await db_parser(cursor)
+    return result
 
 
 async def update_note(title: str, description: str, current_user: str):
